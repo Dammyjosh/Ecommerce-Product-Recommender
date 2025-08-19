f6320e9d6184138258f0ed654bf642eaa8cf7224
Dammyjosh <damton1@yahoo.co.uk>
2025-08-19 07:55:49 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="FLIPKART RECOMMENDER",
+    version="0.1",
+    author="Dammyjosh",
+    packages=find_packages(),
+    install_requires = requirements,
+)
+
+
+
